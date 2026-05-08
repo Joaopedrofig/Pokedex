6a47bcac000dd82a98366ffd977a2e353a27343c
João Pedro da Costa Figueiredo <joaocosta@fuvs.br>
2026-05-07 21:27:52 -0300

    feat: estrutura inicial da pokedex

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p>Desenvolvido por João Pedro da Costa Figueiredo — Programação Frontend — UNIVAS</p>
+    </footer>
+  )
+}
+
+export default Footer
